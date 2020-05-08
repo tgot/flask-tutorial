691fd23c24c32a462b769319d877d99b4fedabf7
Andrej Cibej <andrej.cibej@gmail.com>
2020-05-08 14:29:57 +0200

    interim

--- flaskr/db.py
+++ flaskr/db.py
@@ -29,10 +29,10 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
-
 
 
 @click.command('init-db')
